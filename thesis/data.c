82cdf7c1896f7fb917c5a0d017a5ac5cee17b4ee
Kavit Shah <kavit.shah8@gmail.com>
2014-09-13 23:09:51 -0400

    adds one more exampl of pointers

--- thesis/data.c
+++ thesis/data.c
@@ -1,9 +1,20 @@
+/*
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
 
-int main(){
-	srand(time(NULL));
+int main2(){
+	//srand(time(NULL));
+	int* ptr;
+	int** ptr_ptr;
+	ptr = (int* )malloc(sizeof(int));
+	printf("value of ptr %d \n", ptr);
+	ptr_ptr = (int** )malloc(sizeof(int));
+	ptr_ptr = &ptr;
+	**ptr_ptr = 10;
+	printf("value of ptr after derencing %d \n", ptr);
 
+	free(ptr);
+	free(ptr_ptr);
 	return 0;
-}
+}*/
