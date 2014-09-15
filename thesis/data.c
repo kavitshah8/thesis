920e71e5a4e27e041d58d8d6463242c620131e36
kavitshah8 <kavit.shah8@gmail.com>
2014-09-15 17:22:50 -0400

    making it look preety

--- thesis/data.c
+++ thesis/data.c
@@ -31,21 +31,30 @@ int main(){
 	iterator = root;
 	
 	while( N > 0 ){
+		
 		iterator->num_children = rand() % ( N + 1 );
+		
 		if( iterator == root && iterator->num_children == 0 ){
 			iterator->num_children = 1;
 		}
+		
 		N = N - ( iterator->num_children );
+		
 		iterator->arr = (struct node**)malloc( sizeof(struct node*) * iterator->num_children ) ;
+		
 		for( i=0; i<iterator->num_children; i++ ){
+		
 			iterator->arr[i] = (struct node*)malloc( sizeof(struct node) ) ;
 			queue[back] = iterator->arr[i];
 			back++;
+		
 		}
+		
 		if( front > back ){
 			printf("Queue is not working as supposed to \n");
 			break;
 		}
+		
 		iterator = queue[front];
 		front++;
 	}
