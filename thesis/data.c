15586536267af38a0440a8edfbe49b8938b76b4e
Kavit Shah <kavit.shah8@gmail.com>
2014-09-15 00:10:41 -0400

    bfs implementation

--- thesis/data.c
+++ thesis/data.c
@@ -2,8 +2,8 @@
 #include<stdlib.h>
 #include<time.h>
 
-struct node {
-	int children;
+struct node{
+	int num_children;
 	struct node** arr;
 };
 
@@ -13,22 +13,40 @@ struct node* makenode(){
 
 int main(){
 	int N = 12;
+
 	struct node* root;
+	struct node* iterator;
+	struct node** queue;
 	int i,j;
+	int front, back;
+
+	front = 0 ;
+	back = 0;
+	queue = (struct ndoe**)malloc(sizeof(struct node*)*N);
 
 	srand(time(NULL));
 
 	N--;
 	root = makenode();
-	root->children = ( 1 + rand() ) % ( N + 1 );
-	N = N - ( root->children );
-	root->arr = (struct node**)malloc( sizeof(struct node) * root->children ) ;
-
-	for( i = 0; i < (root->children); i++){
-		j = rand() % ( N + 1 );
-		N = N - j;
-		root->arr = (struct node**)malloc( sizeof(struct node) * j );
-		root->arr++;
+	iterator = root;
+	
+	while( N > 0 ){
+		iterator->num_children = rand() % ( N + 1 );
+		if( iterator == root && iterator->num_children == 0 ){
+			iterator->num_children = 1;
+		}
+		N = N - ( iterator->num_children );
+		iterator->arr = (struct node**)malloc( sizeof(struct node*) * iterator->num_children ) ;
+		for( i=0; i<iterator->num_children; i++ ){
+			queue[back] = iterator->arr[i];
+			back++;
+		}
+		if( front > back ){
+			printf("Queue is not working as supposed to \n");
+			break;
+		}
+		iterator = queue[front];
+		front++;
 	}
 
 //	free(root->arr);
