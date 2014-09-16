519bb7f9dcb15c35af49e3022080b46cc7613276
kavitshah8 <kavit.shah8@gmail.com>
2014-09-15 23:25:06 -0400

    more pointers details

--- thesis/data.c
+++ thesis/data.c
@@ -27,30 +27,44 @@ int main(){
 
 	srand(time(NULL));
 
-	// makenode() retuens a pointer to the ( struct node ), root is pointing to that pointer.
-	// root is not directly pointing to ( struct node )
+	/* makenode() returns a pointer to the ( struct node ), root is pointing to that pointer.
+		---------------------------------------------------
+		|ADDRESS| NAME | VALUE|     |ADDRESS| NAME | VALUE|
+		| 6001  | root | 3001 | 	| 3001  | ???? | ???? | 
+		----------------------------------------------------
+	*/
 	root = makenode();
 	iterator = root;
 	N--;	
 	
 	while( N > 0 ){
+
+		// (*iterator).num_children = iterator->num_children
+		// second one is called indirect component selection
 		iterator->num_children = rand() % ( N + 1 );
+		
 		if( iterator == root && iterator->num_children == 0 ){
 			iterator->num_children = 1;
 		}
+		
 		N = N - ( iterator->num_children );
+		
 		iterator->arr = (struct node**)malloc( sizeof(struct node*) * iterator->num_children ) ;
+		
 		for( i=0; i<iterator->num_children; i++ ){
 			iterator->arr[i] = (struct node*)malloc( sizeof(struct node) ) ;
 			queue[back] = iterator->arr[i];
 			back++;
 		}
+		
 		if( front > back ){
 			printf("Queue is not working as supposed to \n");
 			break;
 		}
+		
 		iterator = queue[front];
 		front++;
+	
 	}
 
 //	free(root->arr);
