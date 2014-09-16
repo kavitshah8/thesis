73eabf18cdf8f8c507a0f27f6b59d3a9b709c97a
kavitshah8 <kavit.shah8@gmail.com>
2014-09-15 21:34:06 -0400

    reason for double pointers in structure

--- thesis/data.c
+++ thesis/data.c
@@ -4,6 +4,7 @@
 
 struct node{
 	int num_children;
+	// To store the address of (struct node) but malloc returns (struct node**)
 	struct node** arr;
 };
 
@@ -22,40 +23,30 @@ int main(){
 
 	front = 0 ;
 	back = 0;
-	
 	queue = (struct node**)malloc(sizeof(struct node*)*N);
 
 	srand(time(NULL));
 
+	N--;
 	root = makenode();
 	iterator = root;
-	N--;
 	
 	while( N > 0 ){
-		
 		iterator->num_children = rand() % ( N + 1 );
-		
 		if( iterator == root && iterator->num_children == 0 ){
 			iterator->num_children = 1;
 		}
-		
 		N = N - ( iterator->num_children );
-		
 		iterator->arr = (struct node**)malloc( sizeof(struct node*) * iterator->num_children ) ;
-		
 		for( i=0; i<iterator->num_children; i++ ){
-		
 			iterator->arr[i] = (struct node*)malloc( sizeof(struct node) ) ;
 			queue[back] = iterator->arr[i];
 			back++;
-		
 		}
-		
 		if( front > back ){
 			printf("Queue is not working as supposed to \n");
 			break;
 		}
-		
 		iterator = queue[front];
 		front++;
 	}
