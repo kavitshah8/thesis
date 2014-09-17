93aef8371e80e0aa97bf8ae14cd38709006c57ec
kavitshah8 <kavit.shah8@gmail.com>
2014-09-17 14:17:47 -0400

    changes logic for front

--- thesis/tree.c
+++ thesis/tree.c
@@ -5,7 +5,7 @@
 struct node{
 	int num_children;
 	int depth;
-	// To store the address of (struct node) but malloc returns (struct node*)
+	// To store the address of (struct node) and malloc returns (struct node*) which is address of (struct node)
 	struct node** arr;
 };
 
@@ -22,7 +22,7 @@ int main(){
 	int i,j;
 	int front, back;
 
-	front = 0 ;
+	front = 0;
 	back = 0;
 	queue = (struct node**)malloc(sizeof(struct node*)*N);
 
@@ -36,15 +36,15 @@ int main(){
 	*/
 	root = makenode();
 	iterator = root;
+	//(*iterator).depth = iterator->depth ( direct = indirect component selection )
 	iterator->depth = 0;
+	// queue is an array and you can store (struct node*) in it
 	queue[back] = iterator;
 	back++;
 	N--;	
 	
 	while( N > 0 ){
 
-		// (*iterator).num_children = iterator->num_children
-		// second one is called indirect component selection
 		iterator->num_children = rand() % ( N + 1 );
 		
 		if( iterator == root && iterator->num_children == 0 ){
@@ -55,20 +55,22 @@ int main(){
 		
 		iterator->arr = (struct node**)malloc( sizeof(struct node*) * iterator->num_children ) ;
 		
-		for( i=0; i<iterator->num_children; i++ ){
+		for( i=0; i < iterator->num_children; i++ ){
+			
 			iterator->arr[i] = (struct node*)malloc( sizeof(struct node) ) ;
 			iterator->arr[i]->depth = iterator->depth + 1;
 			queue[back] = iterator->arr[i];
 			back++;
+		
 		}
 		
 		if( front > back ){
 			printf("Queue is not working as supposed to \n");
 			break;
 		}
-		
-		iterator = queue[front];
+
 		front++;
+		iterator = queue[front];
 	
 	}
 
