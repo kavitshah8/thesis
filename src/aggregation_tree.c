9c0988e4ea54890df6303d4e5df62d43126ead7a
kavitshah8 <kavit.shah8@gmail.com>
2014-10-25 17:16:27 -0400

    divides the src files in different files according to their functions

---
+++ src/aggregation_tree.c
@@ -0,0 +1,61 @@
+#include "../header_files/util.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int countTree (atn *head)
+{
+	int i, sum;
+	int numChildren;
+
+	sum = 1;
+	
+	if (!head)
+		return 0;
+
+	numChildren = head->numChildren;
+
+	for ( i=0; i < numChildren; i++)
+	{
+		sum += countTree(head->arr[i]);
+	}
+	
+	return sum;
+}
+
+int depthOfNode (atn *head, atn *ptr)
+{
+	int dep = 0;
+	
+	while (ptr != head)
+	{
+		ptr = ptr->parent;
+		dep++;
+	}
+	
+	return dep;
+}
+
+void printTree (atn *iterator)
+{
+	int i = 0;
+	
+	if (iterator != NULL)
+	{
+		for (i = 0; i < iterator->numChildren; i++)
+		{
+			// try implementing without recurssion
+			printTree(iterator->arr[i]);
+		}
+
+		if (iterator->depth == 0)
+		{
+			printf("id = %d, depth = %d, numChildren = %d, parent = NULL\n", iterator->id, iterator->depth, iterator->numChildren );
+		}
+		else
+		{
+			printf("id = %d, depth = %d, numChildren = %d, parent's_id = %d\n ", iterator->id, iterator->depth, iterator->numChildren, iterator->parent->id );
+		}
+
+	}	
+}
+
